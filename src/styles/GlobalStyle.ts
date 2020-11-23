e2d82c0542b2ee64e34a73eafd81e18b041809c4
felipekafuri <felipe11.rk@gmail.com>
2020-11-23 14:41:20 -0300

    Initial commit

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+  }
+
+  body{
+    background: #fafafa;
+    color: #000000;
+    font-family: Roboto, Helvetica, sans-serif
+  }
+`
